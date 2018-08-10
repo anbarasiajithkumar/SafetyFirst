e1ff3389b6f5ccbef0804cc472202dfd8def57f7
anbarasi <anbarasi@MAC-LAP-113.macrosoftindia.com>
2018-08-10 16:41:17 +0530

    Pass case

--- src/Login/Validuser.java
+++ src/Login/Validuser.java
@@ -35,10 +35,6 @@ public class Validuser {
 		driver.findElement(By.id("password")).sendKeys("Test@123");
 		driver.findElement(By.cssSelector(".btn.btn-login")).click();
 		Thread.sleep(2000);
-		String actualTitle = driver.getTitle();
-		String expectedTitle = "My Title";
-		assertEquals(actualTitle, expectedTitle);
-		Thread.sleep(2000);
 		driver.close();
 	}
 
